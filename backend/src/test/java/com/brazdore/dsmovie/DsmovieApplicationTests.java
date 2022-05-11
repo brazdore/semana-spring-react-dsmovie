9ddb1572ec59db72ad9bb0f9ec3e8de197702fab
Brian dos Reis <brazdore@gmail.com>
2022-05-11 00:03:49 -0300

    first commit

---
+++ backend/src/test/java/com/brazdore/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brazdore.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
