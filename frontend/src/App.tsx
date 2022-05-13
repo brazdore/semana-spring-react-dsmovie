900e9e474c8fedab195c81b25bebd8374dc70b69
Brian dos Reis <brazdore@gmail.com>
2022-05-13 03:51:05 -0300

    Devolvendo o projeto de frontend sem uppercase

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "pages/listing";
+import Form from "pages/form";
+import Navbar from "components/navbar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
