a536073a3baa048644ec0f6580658d1b0633c1ec
Brian dos Reis <brazdore@gmail.com>
2022-05-13 04:07:52 -0300

    Alterando nome das imagens & upando react-app-env.d.ts

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as GithubIcon } from "assets/img/githubicon.svg";
+import { ReactComponent as GithubIcon } from "assets/img/github-icon.svg";
 import "./styles.css";
 
 function Navbar() {
