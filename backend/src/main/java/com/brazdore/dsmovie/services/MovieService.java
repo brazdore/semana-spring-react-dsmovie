b95c3229dd7d26a00a77f51ad6105aed4d91bce1
Brian dos Reis <brazdore@gmail.com>
2022-05-14 17:07:04 -0300

    Alterando findAll() devido a problema no retorno do Page<>

--- backend/src/main/java/com/brazdore/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/brazdore/dsmovie/services/MovieService.java
@@ -3,12 +3,9 @@ package com.brazdore.dsmovie.services;
 import com.brazdore.dsmovie.dtos.MovieDTO;
 import com.brazdore.dsmovie.repositories.MovieRepository;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
@@ -21,15 +18,14 @@ public class MovieService {
 
     @Transactional(readOnly = true)
     public Page<MovieDTO> findAll(Pageable pageable) {
-        return new PageImpl<>(
-                repository.findAll(pageable).stream()
-                        .parallel()
-                        .map(MovieDTO::new)
-                        .collect(Collectors.toList()));
+        return repository.findAll(pageable)
+                .map(MovieDTO::new);
     }
 
     @Transactional(readOnly = true)
     public MovieDTO findById(Long id) {
-        return repository.findById(id).map(MovieDTO::new).orElse(null);
+        return repository.findById(id)
+                .map(MovieDTO::new)
+                .orElse(null);
     }
 }
