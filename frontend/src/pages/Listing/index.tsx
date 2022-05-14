ec7ca070aaea71a0ea46eb8cd922b6b349e73a50
Brian dos Reis <brazdore@gmail.com>
2022-05-14 16:19:02 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,32 +1,42 @@
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import axios from "axios";
+import {BASE_URL} from "../../utils/requests";
 
 function Listing() {
-  return (
-    <>
-      <Pagination />
 
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    // Errado!
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data)
+        });
+
+
+    return (
+        <>
+            <Pagination/>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Listing;
